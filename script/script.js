7400e8d3572562dd338153a892341884773f7fbf
Mrpeej <rsantosdasilva05@gmail.com>
2023-10-04 13:53:03 -0300

    primeiro commit

--- script/script.js
+++ script/script.js
@@ -2,16 +2,15 @@ function mult()
 {
     let num = document.getElementById("caixaNum").value;
 
-    let resul = document.getElementById("observa").value;
+    let resul="";
 
-    document.write(+num+" x 1 ="+num*1+"<br>"
-    +num+" x 2 ="+num*2+"<br>"
-    +num+" x 3 ="+num*3+"<br>"
-    +num+" x 4 ="+num*4+"<br>"
-    +num+" x 5 ="+num*5+"<br>"
-    +num+" x 6 ="+num*6+"<br>"
-    +num+" x 7 ="+num*7+"<br>"
-    +num+" x 8 ="+num*8+"<br>"
-    +num+" x 9 ="+num*9+"<br>"
-    +num+" x 10 ="+num*10+"<br>");
+    let i;
+
+    for(i=1; i<=10; i++){
+    resul+=+num+" x "+i+" = "+num*i+"<br>";
+    }
+
+    document.getElementById("resultado").innerHTML=resul;
+    // .innetHTML da pra atribuir uma variavel a ele, escrevendo seu valor
+    
 }       
